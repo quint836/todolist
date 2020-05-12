aa99a7c46a54e6700e396da214d62dc1720b77da
Quint6402 <56156129+Quint6402@users.noreply.github.com>
2020-05-13 02:25:21 +0300

    добавил в toolbar конку настроек

--- SamsungProj2/app/src/main/java/com/arhiser/todolist/screens/main/MainActivity.java
+++ SamsungProj2/app/src/main/java/com/arhiser/todolist/screens/main/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
